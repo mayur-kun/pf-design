5acd2ae02ca80f0e7c0914de27d8d384898d123c
Mayur Parab <mayur.parab1223@gmail.com>
2024-08-20 22:33:26 +0530

    final

--- script.js
+++ script.js
@@ -33,9 +33,9 @@ function updateTimeAndWeather() {
     const timeString = now.toLocaleTimeString('en-IN', { hour: '2-digit', minute: '2-digit', hour12: false, timeZone: 'Asia/Kolkata' });
 
     // Fetch weather data
-    const apiKey = os.environ.get('WEATHER_API_KEY');
+    const apiKey = '95280c8d75d797f82334f416853739be';
     // const city = 'Mumbai';
-    
+
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=19.07&lon=72.87&appid=${apiKey}`)
         .then(response => response.json())
         .then(data => {
